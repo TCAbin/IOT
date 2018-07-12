db3178f8c2ca8350623a21d1cb5b0864ee27bdb4
TCAbin <1003006168@qq.com>
2018-07-12 16:43:32 +0800

    完成

---
+++ src/main/java/cn/com/gree/utils/DateTransform.java
@@ -0,0 +1,22 @@
+package cn.com.gree.utils;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateTransform {
+
+    /**
+     * @autor 260172
+     * @date 2018/6/26 14:53
+     * 增加hour小时
+     */
+    public static Date conver (Date date,int hour){
+        if(hour == 12){
+            return date;
+        }
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.HOUR, hour);
+        return cal.getTime();
+    }
+}
